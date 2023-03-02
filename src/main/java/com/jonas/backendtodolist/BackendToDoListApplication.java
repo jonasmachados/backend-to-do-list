bf91aa404520a2e6b5439af6afa330e67eb72da1
jonasmachados <jonasmachado.ti@gmail.com>
2023-03-02 10:31:34 -0300

    Created Project

---
+++ src/main/java/com/jonas/backendtodolist/BackendToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.backendtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/backendtodolist/BackendToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.backendtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
