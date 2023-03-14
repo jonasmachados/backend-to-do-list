dacf1ec94a87721809bc4d96e50748ee884cfa8a
jonasmachados <jonasmachado.ti@gmail.com>
2023-03-14 11:25:28 -0300

    Creating REST Request DELETE a ToDoList

--- src/main/java/com/jonas/backendtodolist/services/ToDoListService.java
+++ src/main/java/com/jonas/backendtodolist/services/ToDoListService.java
@@ -2,10 +2,13 @@ package com.jonas.backendtodolist.services;
 
 import com.jonas.backendtodolist.entity.ToDoList;
 import com.jonas.backendtodolist.repositories.ToDoListRepository;
+import com.jonas.backendtodolist.services.exceptions.DatabaseException;
 import com.jonas.backendtodolist.services.exceptions.ResourceNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,6 +28,16 @@ public class ToDoListService {
 
     public ToDoList insert(ToDoList obj) {
         return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
 }
