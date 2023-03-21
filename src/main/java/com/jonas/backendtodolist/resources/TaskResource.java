158d532d483b13ff9d719e1129fa8793bd04bb41
jonasmachados <jonasmachado.ti@gmail.com>
2023-03-21 10:41:09 -0300

    Creating REST Request POST to a new Task

--- src/main/java/com/jonas/backendtodolist/resources/TaskResource.java
+++ src/main/java/com/jonas/backendtodolist/resources/TaskResource.java
@@ -1,14 +1,19 @@
 package com.jonas.backendtodolist.resources;
 
+import com.jonas.backendtodolist.entity.Task;
 import com.jonas.backendtodolist.projection.TaskByToDoListProjection;
 import com.jonas.backendtodolist.services.TaskService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/task")
@@ -22,4 +27,13 @@ public class TaskResource {
         List<TaskByToDoListProjection> list = service.findTaskByToDoList(id);
         return ResponseEntity.ok().body(list);
     }
+    
+    @PostMapping
+    public ResponseEntity<Task> insert(@RequestBody Task obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+        
+    } 
 }
--- src/main/java/com/jonas/backendtodolist/services/TaskService.java
+++ src/main/java/com/jonas/backendtodolist/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.jonas.backendtodolist.services;
 
+import com.jonas.backendtodolist.entity.Task;
 import com.jonas.backendtodolist.projection.TaskByToDoListProjection;
 import com.jonas.backendtodolist.repositories.TaskRepository;
 import java.util.List;
@@ -8,12 +9,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
-  
+
     @Autowired
     private TaskRepository repository;
-    
+
     public List<TaskByToDoListProjection> findTaskByToDoList(long id) {
         return repository.findTaskByToDoList(id);
     }
-    
+
+    public Task insert(Task obj) {
+        return repository.save(obj);
+    }
+
 }
