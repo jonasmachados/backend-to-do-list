118378b53143c624ee1615230ac46cf5757657f5
jonasmachados <jonasmachado.ti@gmail.com>
2023-06-29 10:52:59 -0300

    Added Maven plugins for testing and code coverage

--- src/test/java/com/jonas/backendtodolist/BackendToDoListApplicationTests.java
+++ src/test/java/com/jonas/backendtodolist/BackendToDoListApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendToDoListApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void main() {
+        BackendToDoListApplication.main(new String[]{});
+    }
 
 }
